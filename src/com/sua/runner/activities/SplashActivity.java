6bfe908187f155f63c9729cff4923bee8c6473c1
Anastasiia Sukhanovska <stacy4313@gmail.com>
2014-03-17 19:17:57 +0100

    Created main activity with tabs and view pager

--- src/com/sua/runner/activities/SplashActivity.java
+++ src/com/sua/runner/activities/SplashActivity.java
@@ -1,8 +1,10 @@
-package com.sua.runner;
+package com.sua.runner.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import com.sua.runner.R;
 
 
 public class SplashActivity extends Activity {
@@ -18,8 +20,8 @@ public class SplashActivity extends Activity {
 
             @Override
             public void run() {
-                //Intent i = new Intent(MainSplashScreen.this, FirstScreen.class);
-                //startActivity(i);
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
                 finish();
             }
         }, SPLASH_DISPLAY_TIME);
---
+++ src/com/sua/runner/fragments/NewRunFragment.java
@@ -0,0 +1,24 @@
+package com.sua.runner.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.sua.runner.R;
+
+public class NewRunFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_run, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
