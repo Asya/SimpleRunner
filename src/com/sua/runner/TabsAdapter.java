25ca2215110320e8390e1f82943bc19777269350
Anastasiia Sukhanovska <stacy4313@gmail.com>
2014-03-26 23:15:38 +0100

    Code cleaning

--- src/com/sua/runner/TabsAdapter.java
+++ src/com/sua/runner/TabsAdapter.java
@@ -3,7 +3,6 @@ package com.sua.runner;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
@@ -60,7 +59,7 @@ public class TabsAdapter extends FragmentPagerAdapter implements ActionBar.TabLi
     }
 
     public int getFragmentPosition(Class<?> clss) {
-        for (int i=0; i<mTabs.size(); i++) {
+        for (int i = 0; i < mTabs.size(); i++) {
             if (mTabs.get(i).clss == clss) {
                 return i;
             }
@@ -72,8 +71,8 @@ public class TabsAdapter extends FragmentPagerAdapter implements ActionBar.TabLi
     public Fragment getItem(int position) {
         TabInfo info = mTabs.get(position);
 
-        for(Fragment fragment : fragments) {
-            if(fragment.getClass().getName().equals(info.clss.getName())) {
+        for (Fragment fragment : fragments) {
+            if (fragment.getClass().getName().equals(info.clss.getName())) {
                 return fragment;
             }
         }
@@ -103,15 +102,12 @@ public class TabsAdapter extends FragmentPagerAdapter implements ActionBar.TabLi
     @Override
     public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
         Object tag = tab.getTag();
-        for (int i=0; i<mTabs.size(); i++) {
+        for (int i = 0; i < mTabs.size(); i++) {
             if (mTabs.get(i) == tag) {
-                //updateDatasetMovies (i);
                 mViewPager.setCurrentItem(i);
             }
         }
     }
-
-
 
     @Override
     public void onTabReselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
@@ -125,110 +121,4 @@ public class TabsAdapter extends FragmentPagerAdapter implements ActionBar.TabLi
 
     }
 
-
-//    public void updateDatasetMovies (int pos)
-//    {
-//        //Let's update the dataset for the selected genre
-//        TabFragment fragment =
-//                (TabFragment) ((FragmentActivity)mContext).getSupportFragmentManager().findFragmentByTag(
-//                        "android:switcher:"+R.id.pager+":"+pos);
-//
-//        //TabFragment fragment = (TabFragment) getItem(pos);
-//        if(fragment != null)  // could be null if not instantiated yet
-//        {
-//            if(fragment.getView() != null)
-//            {
-//                // no need to call if fragment's onDestroyView()
-//                //has since been called.
-//                fragment.updateDisplay(pos); // do what updates are required
-//            }
-//        }
-
-    }
-
-//public class TabsAdapter extends FragmentPagerAdapter
-//        implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
-//    private final Context mContext;
-//    private final ActionBar mActionBar;
-//    private final ViewPager mViewPager;
-//    private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
-//
-//    static final class TabInfo {
-//        private final Class<?> clss;
-//        private final Bundle args;
-//
-//        TabInfo(Class<?> _class, Bundle _args) {
-//            clss = _class;
-//            args = _args;
-//        }
-//    }
-//
-//    public TabsAdapter(Activity activity, ViewPager pager) {
-//        super(activity.getFragmentManager());
-//        mContext = activity;
-//        mActionBar = activity.getActionBar();
-//        mViewPager = pager;
-//        mViewPager.setAdapter(this);
-//        mViewPager.setOnPageChangeListener(this);
-//    }
-//
-//    public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
-//        TabInfo info = new TabInfo(clss, args);
-//        tab.setTag(info);
-//        tab.setTabListener(this);
-//        mTabs.add(info);
-//        mActionBar.addTab(tab);
-//        notifyDataSetChanged();
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return mTabs.size();
-//    }
-//
-//    @Override
-//    public Fragment getItem(int position) {
-//        TabInfo info = mTabs.get(position);
-//        return Fragment.instantiate(mContext, info.clss.getName(), info.args);
-//    }
-//
-//    @Override
-//    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//    }
-//
-//    @Override
-//    public void onPageSelected(int position) {
-//        mActionBar.setSelectedNavigationItem(position);
-//    }
-//
-//    @Override
-//    public void onPageScrollStateChanged(int state) {
-//    }
-//
-//    @Override
-//    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-//        Object tag = tab.getTag();
-//        for (int i=0; i<mTabs.size(); i++) {
-//            if (mTabs.get(i) == tag) {
-//                mViewPager.setCurrentItem(i);
-//            }
-//        }
-//    }
-//
-//    public int getFragmentPosition(Class<?> clss) {
-//        for (int i=0; i<mTabs.size(); i++) {
-//            if (mTabs.get(i).clss == clss) {
-//                return i;
-//            }
-//        }
-//        return -1;
-//    }
-//
-//    @Override
-//    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-//    }
-//
-//    @Override
-//    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-//    }
-//}
+}
